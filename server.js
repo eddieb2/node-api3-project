dfd13f645b06f056d011ac6002a35a52f860e554
Edward Blanciak <blanciak.edward2@gmail.com>
2020-04-08 20:05:14 -0400

    logger compelte

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger);
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+// Custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  let date = new Date();
+  console.log(`${req.method} request to ${req.originalUrl} at ${date}`);
+  next();
+}
 
 module.exports = server;
